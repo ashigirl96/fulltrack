f134c4fed315dfcbf0fbcd65c9b102fd5561ac26
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2022-08-28 15:03:33 +0900

    Impl: add eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,21 @@
 module.exports = {
   root: true,
-  plugins: ['react', 'react-hooks'],
+  plugins: ['react', 'react-hooks', '@typescript-eslint'],
   env: {
     es2020: true,
   },
-  extends: ['eslint:recommended', 'plugin:react-hooks/recommended'],
+  extends: [
+    'eslint:recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+  ],
   rules: {
     'no-console': 'off',
     'no-undef': 'off',
     'react/jsx-uses-vars': 1,
+    'no-unused-vars': 'off',
+    '@typescript-eslint/no-unused-vars': ['error'],
   },
   parser: '@typescript-eslint/parser',
 }
