59c412cce5137876d2ac59de927989bf6a688ae2
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2022-08-29 19:20:17 +0900

    yarn add recoilize

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,20 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { RecoilRoot } from 'recoil'
+import dynamic from 'next/dynamic'
+import { useEffect, useState } from 'react'
+const RecoilizeDebugger = dynamic(() => import('recoilize'), { ssr: false })
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const [root, setRoot] = useState<HTMLElement | null>(null)
+  useEffect(() => {
+    if (typeof window.document !== 'undefined') {
+      setRoot(document.getElementById('__next'))
+    }
+  }, [root])
   return (
     <RecoilRoot>
+      <RecoilizeDebugger root={root} />
       <Component {...pageProps} />
     </RecoilRoot>
   )
