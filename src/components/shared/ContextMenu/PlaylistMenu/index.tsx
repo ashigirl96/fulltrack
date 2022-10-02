af2101c710284002deb9485a11db11e1348c1904
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2022-10-02 21:05:45 +0900

    Impl redirect to list when current playlist deleted

--- src/components/shared/ContextMenu/PlaylistMenu/index.tsx
+++ src/components/shared/ContextMenu/PlaylistMenu/index.tsx
@@ -1,17 +1,26 @@
-import React from 'react'
+import React, { useCallback } from 'react'
 import { useDeleteUserPlaylistCollection } from '@/hooks/playlist'
 import { PlaylistStoreId } from '@/atoms/firestore/playlist'
+import { useRouter } from 'next/router'
 
 type Props = {
   playlistId: PlaylistStoreId
 }
 export function PlaylistMenu({ playlistId }: Props) {
+  const router = useRouter()
   const deleteUserPlaylist = useDeleteUserPlaylistCollection(playlistId)
-  // TODO: 削除されるプレイリストにいるとき、/listに遷移する
+  const handleClickDelete = useCallback(async () => {
+    await deleteUserPlaylist()
+    const currentPlaylistId = router.query['playlistId']
+    // 削除したパスと同じだった場合、ルートに飛ばす
+    if (currentPlaylistId && currentPlaylistId === playlistId) {
+      await router.push(`/playlists/list`)
+    }
+  }, [deleteUserPlaylist, playlistId, router])
   return (
     <>
       <li>
-        <button onClick={deleteUserPlaylist}>削除</button>
+        <button onClick={handleClickDelete}>削除</button>
       </li>
       <li>
         <a>名前を変更</a>
