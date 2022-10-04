a7931a42b7cc61556a79184072332bc2dea816a2
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2022-10-05 00:38:14 +0900

    Impl: create playlist from context menu

--- src/components/shared/ContextMenu/PlaylistMenu/index.tsx
+++ src/components/shared/ContextMenu/PlaylistMenu/index.tsx
@@ -3,6 +3,9 @@ import { useDeleteUserPlaylistCollection } from '@/hooks/playlist'
 import { PlaylistStoreId } from '@/atoms/firestore/playlist'
 import { useRouter } from 'next/router'
 import { useSetIsEditPlaylistName } from '@/atoms/contextMenu/states'
+import { useGetCurrentUserId } from '@/hooks/firebaseAuth'
+import { UserId } from '@/types'
+import { useCreateUserPlaylist } from '@/hooks/playlist/useCreateUserPlaylist'
 
 type Props = {
   playlistId: PlaylistStoreId
@@ -20,6 +23,7 @@ export function PlaylistMenu({ playlistId }: Props) {
   }, [deleteUserPlaylist, playlistId, router])
 
   const handleRename = useSetIsEditPlaylistName(playlistId)
+  const currentUserId = useGetCurrentUserId()
 
   return (
     <>
@@ -29,9 +33,17 @@ export function PlaylistMenu({ playlistId }: Props) {
       <li>
         <button onClick={handleRename}>名前を変更する</button>
       </li>
-      <li>
-        <button>プレイリストを作成する</button>
-      </li>
+      {currentUserId && <CreatePlaylist currentUserId={currentUserId} />}
     </>
+  )
+}
+
+type CreateProps = { currentUserId: UserId }
+function CreatePlaylist({ currentUserId }: CreateProps) {
+  const handleClick = useCreateUserPlaylist({ userId: currentUserId })
+  return (
+    <li>
+      <button onClick={handleClick}>プレイリストを作成する</button>
+    </li>
   )
 }
