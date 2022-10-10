a26ac4959ddbff93527e2d99ee97fa0916c391b5
ashigirl96 <reon@mma.cs.tsukuba.ac.jp>
2022-10-10 17:00:40 +0900

    Delete unused

--- src/atoms/youtubePlayer/hooks/index.ts
+++ src/atoms/youtubePlayer/hooks/index.ts
@@ -1,4 +1,3 @@
-export { useSetCurrentVideo } from './useSetCurrentVideo'
 export { useCurrentDurationPercent } from './useCurrentDurationPercent'
 export { useSetNextVideo } from './useSetNextVideo'
 export { useSetPreviousVideo } from './useSetPreviousVideo'
